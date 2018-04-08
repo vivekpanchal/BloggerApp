482803a5fc4c7700da64c3141ca720781c3c9291
vivekpanchal <vivekpanchal64@gmail.com>
2018-04-09 04:27:09 +0530

    Added FireBase Messaging service in the appplication

---
+++ app/src/main/java/com/vivek/betechfreak/MessagingService.java
@@ -0,0 +1,35 @@
+package com.vivek.betechfreak;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        showNotification(remoteMessage.getNotification().getBody());
+    }
+
+    public void showNotification(String message){
+        PendingIntent pi=PendingIntent.getActivity(this,0,new Intent(this,MainActivity.class),0);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("Techie Desi")
+                .setContentText(message)
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+
+    }
+}
